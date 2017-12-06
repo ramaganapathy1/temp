96bfe720d06795a0081e4e8728b088ca7b05dd83
sreeram-0xb5e <sreeram2910@gmail.com>
2017-12-06 23:34:12 +0530

    Index root is done

--- server.py
+++ server.py
@@ -1,11 +1,76 @@
-from flask import Flask,render_template
-
+from flask import Flask, redirect, url_for, request,render_template
 app = Flask(__name__)
+import mysql.connector as mariadb
+import datetime
 
+# UTILITY FUNCTIONS
+def decode(var):
+        if isinstance(var,bytearray):
+                return var.decode('utf-8')
+        if isinstance(var,datetime.date):
+                return str(var)
+        if isinstance(var,int):
+                return var
+        if isinstance(var,str):
+                return var
+        return
+
+def dictfetchall(cursor):
+    desc = cursor.description
+    return [dict(zip([col[0] for col in desc], map(decode,row)))
+            for row in cursor.fetchall()]
+
+########################################################
 
 @app.route('/')
 def index():
-    res=[[['Title 1','Description 1','date'],['Title2','Description 2','date'],['Title3','Description 3','date'],['Title 4','Description 4','date']],[['image1','News headline','news body'],['image2','News Headline 2','News body'],['image2','News Headline 2','News body']],[['person1Image','Name1'],['person2Image','Name2'],['person3Image','Name3']]]
+    #res=[[['Title 1','Description 1','date'],['Title2','Description 2','date'],['Title3','Description 3','date'],['Title 4','Description 4','date']],[['image1','News headline','news body'],['image2','News Headline 2','News body'],['image2','News Headline 2','News body']],[['person1Image','Name1'],['person2Image','Name2'],['person3Image','Name3']]]
+    ann_res = []
+    news_res = []
+    emp_res = []
+    res = []
+    mariadb_connection = mariadb.connect(host="128.199.81.127",user='sree', password='asdf1234asdf', database='landt')
+
+    cursor = mariadb_connection.cursor(prepared = True)
+    cursor.execute("select subject,announcement,date from announcements ORDER BY id DESC LIMIT 4")
+    all_announcement = dictfetchall(cursor)
+
+
+    cursor.execute("select subject,content,iloc from news ORDER BY id DESC LIMIT 4")
+    all_news = dictfetchall(cursor)
+
+
+    cursor.execute("SELECT name,iloc from employee")
+    all_employee = dictfetchall(cursor)
+    cursor.close()
+    mariadb_connection.close()
+
+    for i in all_announcement:
+        temp = []
+        temp.append(str(i['subject']))
+        temp.append(str(i['announcement']))
+        temp.append(str(i['date']))
+        ann_res.append(temp)
+
+    for i in all_news:
+        temp = []
+        temp.append(str(i['iloc']))
+        temp.append(str(i['subject']))
+        temp.append(str(i['content']))
+        news_res.append(temp)
+
+    for i in all_employee:
+        temp = []
+        temp.append(str('iloc'))
+        temp.append(str('name'))
+        emp_res.append(temp)
+
+    res.append(ann_res)
+    res.append(emp_res)
+    res.append(emp_res)
+
+    #return str(res)
+
     return render_template('index.html',li=res)
 
 @app.route('/campusInfo',methods=['GET'])
@@ -40,5 +105,6 @@ def sddc():
 def nammakovai():
     return render_template('sddc.html')
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
